09adbb08c43e95e85ee6c455e46b7c619b6fe3ce
keithryan28 <keithryan28@gmail.com>
2015-06-04 17:01:04 +0100

    update
    added favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
+var favicon = require('express-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -40,6 +41,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
